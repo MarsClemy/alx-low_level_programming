df762bda682b99dc0941fb4bc89cf809743b1a80
NAYO <cnayo2006@gmail.com>
2022-07-01 17:32:20 -0700

    Update combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,23 +7,28 @@
  */
 int main(void)
 {
-	int d = 48, a = 48;
+	int d = 48, a = 48, i = 48;
 
 	while (d < 58)
 	{
 		a = 48;
 		while (a < 58)
 		{
-			if (d != a && d < a)
+			i = 48;
+			while (i < 58)
 			{
-				putchar(d);
-				putchar(a);
-
-				if (!(d == 56 && a == 57))
+				if (d != a && a != i && d != i && d < a && a < i)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(d);
+					putchar(a);
+					putchar(i);
+					if (!(d == 55 && a == 56 && i == 57))
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
+				i++;
 			}
 			a++;
 		}
