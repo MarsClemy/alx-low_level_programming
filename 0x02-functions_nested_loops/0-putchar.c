c112bb14550fa73e601cf32a8897c98fd8c7b6d1
NAYO <cnayo2006@gmail.com>
2022-07-05 21:10:08 -0700

    Print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
-#include <unistd.h>
 #include "main.h"
 /**
  * main - Prints _putchar
  *
- * Return: On Success 0.
+ * Return: Always 0 (Success)
  */
-int main(void);
-int _putchar(char _putchar);
+int main(void)
 {
-	return (write(0, &_putchar, 0));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
